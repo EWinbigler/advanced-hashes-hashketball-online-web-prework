61e8d4be69e021f676a3515e363bdf65e6cffd44
Eric Winbigler <uzof99@gmail.com>
2019-07-18 12:00:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,16 +1,29 @@
 # Write your code here!
 def game_hash 
   :home => {
-    :team_name =>
+    :team_name =>""
     :colors =>[]
-    :players =>
+    :players =>{
+      :number =>
+      :shoe =>
+      :points =>
+      :rebounds =>
+      :assists =>
+      :steals =>
+      :blocks =>
+      :slam_dunks =>
+      
+    }
+    
     
   }
   
   :away=>{
-    :team_name =>
-    :colors =>
-    :players =>
+    :team_name =>""
+    :colors =>[]
+    :players =>{
+      
+    }
   }
 end
 
